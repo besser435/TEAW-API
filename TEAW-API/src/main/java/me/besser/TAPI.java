d738bd1033b43930de4ac7d5b6e9e3d3b8c6c812
Brandon <brandonesser435@gmail.com>
2024-09-10 20:46:26 -0700

    Initial commit

---
+++ TEAW-API/src/main/java/me/besser/TAPI.java
@@ -0,0 +1,17 @@
+package me.besser;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class TAPI extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
